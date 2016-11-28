5fa10e77832aad38c37c7d1dc67b9672c03ffa9f
Maxim Petrovsky <mp@sotal.tv>
2016-11-28 08:57:11 +0300

    'mesg'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   # load_and_authorize_resource
   load_and_authorize_resource :unless => :devise_controller?
   rescue_from CanCan::AccessDenied do |exception|
